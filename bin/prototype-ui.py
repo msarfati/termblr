2c404ad9766a0a109c56299be3e1a97d1daba12a
Michael Sarfati <michael.sarfati@utoronto.ca>
2016-01-18 00:49:59 -0500

    last remains of diffs for termblr

--- bin/prototype-ui.py
+++ bin/prototype-ui.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 from pytumblr import TumblrRestClient
 from objectpath import Tree
+from termblr import renderers
 import urwid
 
 client = TumblrRestClient(
-    consumer_key='iWpkjxQeBaFBpIvHTB0RbP7G5ozicNZ5FQtiMkkoKFkiJ4Cfjb',
-    consumer_secret='JfmKt6EJWOPdNs155kBrVC7AC8YO3V9RVJt73PcuU85E7buGsq',
-    oauth_token='ZoegVG4B9pTAFm3lI86bSueW8KVlr0PKOkxNsh4CqmwIMnLRcE',
-    oauth_secret='AkuR2qM8QNvEm62dQ8JbldxYicQJMvpxdOhfBQPALgsBWLrc7G',
+    'iWpkjxQeBaFBpIvHTB0RbP7G5ozicNZ5FQtiMkkoKFkiJ4Cfjb',
+    'JfmKt6EJWOPdNs155kBrVC7AC8YO3V9RVJt73PcuU85E7buGsq',
+    '7y6XDJDlRxjDAZfSRut6cGg0eWch24fcuAiWaKBM2by0HIXvcf',
+    'CVVnwpo7Hv5s2ULnhaZqPlt42uc3VlWYhEBibQhyzH7xdM6zWv'
 )
 
 
---
+++ termblr/pager.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+from pytumblr import TumblrRestClient
+from objectpath import Tree
+
+class Pager
